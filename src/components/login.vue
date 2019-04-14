2bf49545e7eb99c8d8495ec2bd38045b72d208bf
zhang <1784452772@qq.com>
2019-04-14 14:47:57 +0800

    login组建开发完毕

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,5 +1,26 @@
 <template>
-    $END$
+    <div>
+        <div style="padding: 100px 100px 10px;">
+            <h2 class="form-signin">Please sign in</h2>
+            <form class="form-signin" role="form" action="" method="post">
+                <div class="input-group">
+                    <span class="input-group-addon">用户名：</span>
+                    <input type="text" class="form-control" placeholder="username">
+                </div>
+                <br>
+                <div class="input-group">
+                    <span class="input-group-addon">密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码:</span>
+                    <input type="text" class="form-control" placeholder="password">
+
+                </div>
+                <br>
+                <div class="text-right">
+                    <button class="btn btn-primary" type="submit">登陆</button>
+                    <button class="btn btn-danger" data-dismiss="modal">取消</button>
+                </div>
+            </form>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +30,9 @@
 </script>
 
 <style scoped>
-
+    .form-signin {
+        max-width: 300px;
+        padding: 15px;
+        margin: 0 auto;
+    }
 </style>
